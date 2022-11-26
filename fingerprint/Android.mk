b556607e361acad2a93d9957c808b0f93aa05940
Simon1511 <simon2002.schoenmackers@gmail.com>
2022-11-26 22:28:48 +0100

    a51: Add UdfpsHandler for NightDisplay and ExtraDim
    * Optical fingerprint sensors don't like NightDisplay and ExtraDim since those decrease overall sensor accuracy
    * Turn ExtraDim off and set NightDisplay's color temperature to AOSP's default white balance value if the fingerprint sensor is touched
    
    Change-Id: I6651cd5c8c066102feebfb764184b099c6ad741d

--- fingerprint/Android.mk
+++ fingerprint/Android.mk
@@ -29,6 +29,7 @@ LOCAL_SHARED_LIBRARIES := \
     libhidlbase \
     liblog \
     libutils \
+    libcutils \
     android.hardware.biometrics.fingerprint@2.1 \
     android.hardware.biometrics.fingerprint@2.2 \
     android.hardware.biometrics.fingerprint@2.3
